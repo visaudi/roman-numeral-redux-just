478cd4b412c8371d332721adecfb74c57949cd6c
mmarovich <mmarovich@gmail.com>
2017-10-21 19:32:16 -0500

    tests 6 to VI

--- roman-numeral-converter.test.js
+++ roman-numeral-converter.test.js
@@ -47,3 +47,12 @@ it("translates '5' to 'V'", function () {
     });
 });
 
+it("translates '6' to 'VI'", function () {
+    expect(convertHinduArabicToRomanNumeral({}, {
+        type: "INTEGER_TO_ROMAN",
+        hinduArabicNumber: 6
+    })).toEqual({
+        romanNumber: "VI"
+    });
+});
+
