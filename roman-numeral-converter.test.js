7fe8fba97599a1412f622a9fdf680342be7a0d51
deliacodes <deliacodes@gmail.com>
2017-12-02 17:24:39 -0800

    Tests conversion of Hindu Arabic Numeral 13 to Roman Numeral XIII

--- roman-numeral-converter.test.js
+++ roman-numeral-converter.test.js
@@ -1,10 +1,12 @@
-const {convertHinduArabicToRomanNumeral} = require("./roman-numeral-converter-reducer");
+const {
+    convertHinduArabicToRomanNumeral
+} = require("./roman-numeral-converter-reducer");
 
 it("translates a Hindu Arabic numeral to its Roman numeral equivalent.", function () {
 
     expect(convertHinduArabicToRomanNumeral({}, {
         type: "INTEGER_TO_ROMAN",
-        hinduArabicNumber: 1 
+        hinduArabicNumber: 1
     })).toEqual({
         romanNumber: "I"
     });
@@ -32,7 +34,7 @@ it("translates '3' to 'III'.", function () {
 it("translates '4' to 'IV'", function () {
     expect(convertHinduArabicToRomanNumeral({}, {
         type: "INTEGER_TO_ROMAN",
-	hinduArabicNumber: 4
+        hinduArabicNumber: 4
     })).toEqual({
         romanNumber: "IV"
     });
@@ -57,45 +59,54 @@ it("translates '6' to 'VI'", function () {
 });
 
 it("translates '7' to 'VII'", function () {
-	expect(convertHinduArabicToRomanNumeral({}, {
-			type: "INTEGER_TO_ROMAN",
-			hinduArabicNumber: 7
-	})).toEqual({
-			romanNumber: "VII"
-	});
+    expect(convertHinduArabicToRomanNumeral({}, {
+        type: "INTEGER_TO_ROMAN",
+        hinduArabicNumber: 7
+    })).toEqual({
+        romanNumber: "VII"
+    });
 });
 it("translates '8' to 'VIII'", function () {
-	expect(convertHinduArabicToRomanNumeral({}, {
-			type: "INTEGER_TO_ROMAN",
-			hinduArabicNumber: 8
-	})).toEqual({
-			romanNumber: "VIII"
-	});
+    expect(convertHinduArabicToRomanNumeral({}, {
+        type: "INTEGER_TO_ROMAN",
+        hinduArabicNumber: 8
+    })).toEqual({
+        romanNumber: "VIII"
+    });
 });
 
 it("translates '9' to 'IX'", function () {
-	expect(convertHinduArabicToRomanNumeral({}, {
-			type: "INTEGER_TO_ROMAN",
-			hinduArabicNumber: 9
-	})).toEqual({
-			romanNumber: "IX"
-	});
+    expect(convertHinduArabicToRomanNumeral({}, {
+        type: "INTEGER_TO_ROMAN",
+        hinduArabicNumber: 9
+    })).toEqual({
+        romanNumber: "IX"
+    });
 });
 
 it("translates '10' to 'X'", function () {
-	expect(convertHinduArabicToRomanNumeral({}, {
-		type: "INTEGER_TO_ROMAN",
-		hinduArabicNumber: 10
-	})).toEqual({
-		romanNumber: "X"
-	});
+    expect(convertHinduArabicToRomanNumeral({}, {
+        type: "INTEGER_TO_ROMAN",
+        hinduArabicNumber: 10
+    })).toEqual({
+        romanNumber: "X"
+    });
 });
 
 it("translates '11' to 'XI'", function () {
-    expect(convertHinduArabicToRomanNumeral({},{
+    expect(convertHinduArabicToRomanNumeral({}, {
         type: "INTEGER_TO_ROMAN",
         hinduArabicNumber: 11
     })).toEqual({
-    romanNumber: "XI"
+        romanNumber: "XI"
+    });
+});
+
+it("translates '13' to 'XIII'", function () {
+    expect(convertHinduArabicToRomanNumeral({}, {
+        type: "INTEGER_TO_ROMAN",
+        hinduArabicNumber: 13
+    })).toEqual({
+        romanNumber: "XIII"
     });
 });
